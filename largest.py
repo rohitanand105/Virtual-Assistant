8d6ae6ee0eda27cd3873201406836b1706b315b3
Rohit Anand <rohitanand105@gmail.com>
2023-07-09 21:46:14 +0530

    Largest from 3 numbers

---
+++ largest.py
@@ -0,0 +1,11 @@
+def large(a,b,c):
+    if (a>b) and (b > c):{
+        print(a)
+    }
+    elif (b > a) and (b > c):{
+        print(b)
+    }
+    else:
+        return c;
+
+print(large(45,82,56))
